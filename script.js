863a8becc1fcca7fd1b7291be7c6218024347155
David <5992264+dav166@users.noreply.github.com>
2023-08-25 18:28:02 -0500

    refactor: Clean up integrated code

--- script.js
+++ script.js
@@ -1,205 +1,3 @@
-const gameBoard = Array.from({ length: 20 }, () => Array(10).fill(0));
-let currentTetrimino = null;
-let isGameOver = false;
-let score = 0;
-let lines = 0;
-let level = 1;
-let isPaused = false;
-
-const tetriminoShapes = {
-    'O': [
-      [1, 1],
-      [1, 1]
-    ],
-    'I': [
-      [1, 1, 1, 1]
-    ],
-    'T': [
-      [0, 1, 0],
-      [1, 1, 1]
-    ],
-    'S': [
-      [0, 1, 1],
-      [1, 1, 0]
-    ],
-    'Z': [
-      [1, 1, 0],
-      [0, 1, 1]
-      ],
-    'J': [
-      [1, 0, 0],
-      [1, 1, 1]
-    ],
-    'L': [
-      [0, 0, 1],
-      [1, 1, 1]
-    ]
-};
-  
-function generateRandomTetrimino() {
-    const keys = Object.keys(tetriminoShapes);
-    const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    return {
-        shape: tetriminoShapes[randomKey],
-        x: 5, // Start in the middle of the game board
-        y: 0  // Start at the top of the game board
-    };
-}
-
-function gameLoop() {
-    if (isGameOver || isPaused) return;
-  
-    // Move the Tetrimino down by one square
-    currentTetrimino.y++;
-  
-    // Collision detection and other game logic go here...
-    if(checkCollision(currentTetrimino, gameBoard)) {
-        // Revert the move
-        currentTetrimino.y--;
-        // Lock the Tetrimino and generate a new one
-        lockTetrimino();
-        if (checkCollision(currentTetrimino, gameBoard)) {
-            isGameOver = true;
-            alert("Game Over!");
-        }
-        currentTetrimino = generateRandomTetrimino();
-    }
-
-    // Clear lines and update score
-    clearLines();
-
-    updateScoreboard();
-
-    // Draw the updated state
-    draw();
-  
-    // Run the game loop again after a delay
-    setTimeout(gameLoop, 500 - (level * 50));
-}
-  
-// To start the game
-currentTetrimino = generateRandomTetrimino();
-gameLoop();
-
-function draw() {
-    const gameBoardElement = document.getElementById("game-board");
-    gameBoardElement.innerHTML = "";
-    
-    for (let y = 0; y < 20; y++) {
-      for (let x = 0; x < 10; x++) {
-        const cell = document.createElement("div");
-        cell.classList.add(gameBoard[y][x] ? "filled" : "empty");
-        gameBoardElement.appendChild(cell);
-      }
-    }
-}
-
-function checkCollision(tetrimino, board) {
-    for (let y = 0; y < tetrimino.shape.length; y++) {
-        for (let x = 0; x < tetrimino.shape[y].length; x++) {
-            if (tetrimino.shape[y][x] &&
-                (board[y + tetrimino.y] && board[y + tetrimino.y][x + tetrimino.x]) !== 0) {
-            return true;
-            }
-        }
-    }
-    return false;
-}
-  
-function clearLines() {
-    for (let y = 19; y >= 0; ) {
-        let cleared = false
-        if (gameBoard[y].every(cell => cell !== 0)) {
-            gameBoard.splice(y, 1);
-            gameBoard.unshift(Array(10).fill(0));
-            lines++;
-            score += 10;
-            cleared = true
-        } else {
-            y--;
-        }
-        if (lines % 10 === 0) {
-            level++;
-        }
-        if (cleared) {
-            updateScoreboard();
-        }
-    }
-}
-  
-document.addEventListener("keydown", function(event) {
-    switch(event.keyCode) {
-        case 37: // Left arrow key
-            moveTetrimino(-1, 0);
-            break;
-        case 39: // Right arrow key
-            moveTetrimino(1, 0);
-            break;
-        case 40: // Down arrow key
-            moveTetrimino(0, 1);
-            break;
-        case 38: // Up arrow key
-            rotateTetrimino();
-            break;
-        case 80: // 'P' key
-            isPaused = !isPaused;
-            if (!isPaused) gameLoop(); // Resume game loop if unpaused
-            break;
-    }
-});
-
-document.getElementById("restart-button").addEventListener("click", function() {
-    isGameOver = false;
-    gameBoard = createEmptyBoard(); 
-    score = 0;
-    lines = 0;
-    level = 1;
-    updateScoreboard();
-    gameLoop();
-})
-
-function moveTetrimino(dx, dy) {
-    currentTetrimino.x += dx;
-    currentTetrimino.y += dy;
-
-    if (checkCollision(currentTetrimino, gameBoard)) {
-        // Revert the move
-        currentTetrimino.x -= dx;
-        currentTetrimino.y -= dy;
-    }
-}
-
-function rotateTetrimino() {
-    const originalShape = currentTetrimino.shape;
-
-    // Rotate the Tetrimino 
-    currentTetrimino.shape = rotateShape(currentTetrimino.shape);
-
-    if (checkCollision(currentTetrimino, gameBoard)) {
-        // Revert the rotation
-        currentTetrimino.shape = originalShape;
-    }
-}
-
-function rotateShape(matrix) {
-    const N = matrix.length - 1;
-    let result = matrix.map((row, i) =>
-        row.map((val, j) => matrix[N - j][i])
-    );
-    return result;
-}
-
-function updateScoreboard () {
-    document.getElementById("score-value").textContent = score;
-    document.getElementById("lines-value").textContent = lines;
-    document.getElementById("level-value").textContent = level;
-}
-
-function createEmptyBoard() {
-    return Array.from({ length: 20 }, () => Array(10).fill(0));
-}
-
-// // //
 
 class Tetrimino {
     constructor(shape) {
@@ -210,6 +8,7 @@ class Tetrimino {
 }
 
 class Game {
+
     constructor() {
         this.board = Array.from({ length: 20 }, () => Array(10).fill(0));
         this.score = 0;
@@ -271,30 +70,30 @@ class Game {
         if (this.isGameOver || this.isPaused) return;
     
         // Move the Tetrimino down by one square
-        currentTetrimino.y++;
+        this.currentTetrimino.y++;
   
         // Collision detection and other game logic go here...
-        if(checkCollision(currentTetrimino, gameBoard)) {
-        
+        if (this.checkCollision()) {
             // Revert the move
-            currentTetrimino.y--;
-    
+            this.currentTetrimino.y--;
             // Lock the Tetrimino and generate a new one
-            lockTetrimino();
-            if (checkCollision(currentTetrimino, gameBoard)) {
-                isGameOver = true;
-                alert("Game Over!");
+            this.lockTetrimino();
+            this.currentTetrimino = this.nextTetrimino;
+            this.nextTetrimino = this.randomTetrimino();
+            this.drawNextTetrimino();
+
+            if (this.checkCollision()) {
+                this.showGameOver();
+                return;
             }
-            currentTetrimino = generateRandomTetrimino();
         }
 
         // Clear lines and update score
-        clearLines();
-
-        updateScoreboard();
-
+        this.clearLines();
+        // Update the scoreboard
+        this.updateScoreboard();
         // Draw the updated state
-        draw();
+        this.draw();
 
         setTimeout(() => this.gameLoop(), 500 - (this.level * 50));
     }
@@ -306,9 +105,24 @@ class Game {
 }
 
 // ... Initialization code ...
-// game.drawNextTetrimino(); // Draw the next Tetrimino when appropriate
+const game = new Game();
+game.drawNextTetrimino(); // Draw next Tetrimino when appropriate
+
+// Key mapping
+const keyMap = {
+    37: 'moveLeft'
+    39: 'moveRight'
+    40: 'moveDown'
+    38: 'rotate'
+    80: 'togglePause'
+};
+
+document.addEventListener("keydown", function(event) {
+    const action = keyMap[event.keyCode];
+    if (action) {
+        game[action]();
+    }
+});
 
 document.getElementById("start-pause").addEventListener("click", () => game.togglePause());
 document.getElementById("restart").addEventListener("click", () => game.restart());
-const game = new Game();
-// Initialize, set up listeners, etc
