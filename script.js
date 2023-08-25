a776c5235902a3fc1c09233edc60df45e299c1dc
David <5992264+dav166@users.noreply.github.com>
2023-08-25 16:31:25 -0500

    feat: Generate Tetriminos

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ let isGameOver = false;
 let score = 0;
 let lines = 0;
 let level = 0;
+
+const tetriminoShapes = {
+    'O': [
+      [1, 1],
+      [1, 1]
+    ],
+    'I': [
+      [1, 1, 1, 1]
+    ],
+    // Add other shapes like T, S, Z, J, L
+  };
+  
+  function generateRandomTetrimino() {
+    const keys = Object.keys(tetriminoShapes);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    return {
+      shape: tetriminoShapes[randomKey],
+      x: 5, // Start in the middle of the game board
+      y: 0  // Start at the top of the game board
+    };
+  }
+  
